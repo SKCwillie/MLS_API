3475e4d49ccb50e022f918163c1d9d16196e76b5
SKCwillie <64146253+SKCwillie@users.noreply.github.com>
2022-03-06 17:33:13 -0600

    Update routes.py

--- API/routes.py
+++ API/routes.py
@@ -1,7 +1,12 @@
 from API import app
 from flask import jsonify
+from API.mls_results import get_all_results
 
 
 @app.route('/status', methods=['GET', 'POST'])
 def status():
     return jsonify({'Status': 'UP'})
+
+@app.route('/results/', methods=['GET'])
+def all_results():
+    return jsonify(get_all_results())
